f950de0c0faead4b879d615c0e0193b393beccde
qsyqn1 <virendar08765@gmail.com>
2026-04-21 15:11:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,3 +33,4 @@ git clone --depth=1 https://github.com/timsaya/luci-app-bandix.git package/luci-
 git clone --depth=1 https://github.com/timsaya/openwrt-bandix.git package/openwrt-bandix
 git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
 # git clone -b dev https://github.com/Blueplanet20120/luci-app-romupdate.git
+git clone --depth=1 https://github.com/qsyqn1/luci-app-onlineupgrade.git package/luci-app-onlineupgrade
