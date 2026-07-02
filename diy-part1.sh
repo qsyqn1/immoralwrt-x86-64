16bdcf644f9a9ce7d895c4b0af888480c3745cfa
qsyqn1 <virendar08765@gmail.com>
2026-07-02 09:00:56 +0800

    Fix typo in OpenClash package clone command

--- diy-part1.sh
+++ diy-part1.sh
@@ -48,4 +48,4 @@ git clone --depth=1 https://github.com/timsaya/openwrt-bandix.git package/openwr
 git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
 git clone --depth=1 https://github.com/qsyqn1/luci-app-onlineupgrade.git package/luci-app-onlineupgrade
 # git clone -b dev https://github.com/Blueplanet20120/luci-app-romupdate.git
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash.
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
